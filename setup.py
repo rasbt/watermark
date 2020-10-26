d0862950eb081809c69ada6095370ade4b2bdf53
Bahram Aghaei <aghaee.bahram@gmail.com>
2020-10-27 01:21:38 +0330

    Update the setup.py (#64)
    * Update the setup.py
    * sort imported modules
    * use dedent to indent multi-line string
    
    * make pep8 happy :-)
    
    Co-authored-by: Sebastian Raschka <mail@sebastianraschka.com>

--- setup.py
+++ setup.py
@@ -5,38 +5,42 @@
 #
 # License: BSD 3 clause
 
-from setuptools import setup, find_packages
-import watermark
+from textwrap import dedent
 
+from setuptools import find_packages, setup
+
+from watermark import __version__ as VERSION
 # Single-source version using method 6 from
 # https://packaging.python.org/guides/single-sourcing-package-version/
-VERSION = watermark.__version__
 
 # Also see settings in setup.cfg
 setup(
-    name='watermark',
+    name="watermark",
     version=VERSION,
-    license='newBSD',
-    description=('IPython magic function to print date/time stamps and'
-                 'various system information.'),
-    author='Sebastian Raschka',
-    author_email='mail@sebastianraschka.com',
-    url='https://github.com/rasbt/watermark',
+    license="newBSD",
+    description=(
+        "IPython magic function to print date/time stamps and"
+        "various system information."
+    ),
+    author="Sebastian Raschka",
+    author_email="mail@sebastianraschka.com",
+    url="https://github.com/rasbt/watermark",
     packages=find_packages(exclude=[]),
-    install_requires=['ipython'],
-    long_description="""
-An IPython magic extension for printing date and time stamps, version numbers,
-and hardware information.
+    install_requires=["ipython"],
+    long_description=dedent(
+        """\
+        An IPython magic extension for printing date and time stamps, version
+        numbers, and hardware information.
 
-Contact
-=============
-If you have any questions or comments about watermark,
-please feel free to contact me via
-email: mail@sebastianraschka.com
+        Contact
+        =============
+        If you have any questions or comments about watermark,
+        please feel free to contact me via
+        email: mail@sebastianraschka.com
 
-This project is hosted at https://github.com/rasbt/watermark
+        This project is hosted at https://github.com/rasbt/watermark
 
-The documentation can be found at
-https://github.com/rasbt/watermark/blob/master/README.md
-"""
+        The documentation can be found at
+        https://github.com/rasbt/watermark/blob/master/README.md"""
+    ),
 )
