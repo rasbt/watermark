1b9cdcb951ffb8033973eff12631a2de832b3021
James Myatt <james.myatt@tessella.com>
2020-11-20 11:40:30 +0000

    Use importlib-metadata for own version

--- watermark/version.py
+++ watermark/version.py
@@ -1,6 +1,10 @@
-import pkg_resources
+try:
+    import importlib.metadata as importlib_metadata
+except ImportError:
+    # Running on pre-3.8 Python; use importlib-metadata package
+    import importlib_metadata
 
 try:
-    __version__ = pkg_resources.get_distribution('watermark').version
+    __version__ = importlib_metadata.version('watermark')
 except Exception:
     __version__ = 'unknown'
