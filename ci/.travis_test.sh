42cb98971b34ad6b9f15a5a73bfa4a9a9a7b1fc6
rasbt <mail@sebastianraschka.com>
2021-02-18 10:44:12 -0600

    add appveyor

--- ci/.travis_test.sh
+++ ci/.travis_test.sh
@@ -5,3 +5,5 @@ set -e
 python -c "import IPython; print('IPython %s' % IPython.__version__)";
 python -c "import watermark; print('watermark %s' % watermark.__version__)";
 ipython -c "%load_ext watermark";
+
+PYTHONPATH='.' pytest -sv
